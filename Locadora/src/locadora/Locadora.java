70f8e16e0f29cab40dee820ae782ed208fa6c257
Robertoh98 <robertodebem@hotmail.com>
2017-05-25 16:10:11 -0300

    Driver e Conexão com database

--- Locadora/src/locadora/Locadora.java
+++ Locadora/src/locadora/Locadora.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package locadora;
+
+import conexao.ConnectionManager;
+import java.sql.Connection;
 
 /**
  *
- * @author Roberto
+ * @author Roberto & Joao
  */
 public class Locadora {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Connection con = ConnectionManager.getConnection();
+        
     }
     
 }
