0b183254d4947235eeb301d8418e213798d3c18e
Robertoh98 <robertodebem@hotmail.com>
2017-06-12 20:26:25 -0300

    views Trabalho

--- Locadora/src/View/FilmeView.java
+++ Locadora/src/View/FilmeView.java
@@ -9,17 +9,30 @@ public class FilmeView {
 
     public void add() {
         Filme filme = new Filme();
+                
 
-        filme.setTitulo(Titulo);
+        String titulo = "";
+        Integer tipo = null;
+        Integer qnt = null;
 
-        filme.setTipo(0);
+        if (titulo == "" || tipo == null || qnt == null) {
+            System.out.println("Os valores estão em branco");
+        } else {
+            if (titulo == "") {
+                System.out.println("O titulo está em branco");
+            }
+            if (tipo == null) {
+                System.out.println("O tipo está em branco");
+            } else if (qnt == null) {
+                System.out.println("A quantidade está em branco");
+            } else {
+                filme.setTitulo(titulo);
+                filme.setTipo(tipo);
+                filme.setQuantidade(qnt);
 
-        filme.setQuantidade(0);
-
-        if () {
-            
+                filmeDao.salvar(filme);
+            }
         }
-        filmeDao.salvar(filme);
     }
 
 }
