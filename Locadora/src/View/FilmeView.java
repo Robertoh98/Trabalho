c17ca34c71f89df7a2bb6a6fe55c81588288d50a
Robertoh98 <robertodebem@hotmail.com>
2017-06-24 12:17:39 -0300

    att view

--- Locadora/src/View/FilmeView.java
+++ Locadora/src/View/FilmeView.java
@@ -7,15 +7,18 @@ import javax.swing.JOptionPane;
 public class FilmeView {
 
     FilmeDAO filmeDao = new FilmeDAO();
+    MenuView menu = new MenuView();
 
     public void add() {
-        Filme filme = new Filme();                        
-        
+        Filme filme = new Filme();
+
         String titulo = "";
         Integer tipo = null;
         Integer qnt = null;
-        
-        titulo = JOptionPane.showInputDialog("");
+
+        titulo = JOptionPane.showInputDialog("Qual o titulo do filme");
+        tipo = Integer.parseInt(JOptionPane.showInputDialog("Qual o titulo do filme"));
+        qnt = Integer.parseInt(JOptionPane.showInputDialog("Qual o titulo do filme"));
 
         if (titulo == "" || tipo == null || qnt == null) {
             System.out.println("Os valores estão em branco");
@@ -29,12 +32,37 @@ public class FilmeView {
             } else if (qnt == null) {
                 System.out.println("A quantidade está em branco");
             } else {
-                filme.setTitulo(titulo);
-                filme.setTipo(tipo);
-                filme.setQuantidade(qnt);
+                if (JOptionPane.showConfirmDialog(null, "Deseja realmente adicionar?") == 0) {
+                    filme.setTitulo(titulo);
+                    filme.setTipo(tipo);
+                    filme.setQuantidade(qnt);
 
-                filmeDao.salvar(filme);                
+                    filmeDao.salvar(filme);
+                    JOptionPane.showMessageDialog(null, "Adicionado com sucesso!");
+                    menu.menus("Filme");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Não foi possível adicionar");
+                    menu.menus("Filme");
+                }
+
             }
+        }
+    }
+
+    public void delete(int id) {
+
+        if (JOptionPane.showConfirmDialog(null, "Deseja realmente excluir?") == 0) {
+            try {
+                Filme filme = filmeDao.findOne(id);
+                filmeDao.excluir(filme);
+                JOptionPane.showMessageDialog(null, "Excluido com sucesso!");
+                menu.menus("Filme");
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Não foi possível excluir");
+            menu.menus("Filme");
         }
     }
 
--- Locadora/src/View/MenuView.java
+++ Locadora/src/View/MenuView.java
@@ -15,11 +15,12 @@ public class MenuView {
                 + "3 - Movimentação\n"
                 + "4 - Relatórios\n"
                 + "0 - Finalizar");
-        if (valor.equalsIgnoreCase("")) {
+
+        try {
+            option = Integer.parseInt(valor);
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Você não botou nenhuma alternativa, tente novamente");
             this.menu();
-        } else {
-            option = Integer.parseInt(valor);
         }
 
         switch (option) {
@@ -30,8 +31,10 @@ public class MenuView {
                 this.menus("Filmes");
             case 3:
                 this.menuMovimentação();
+                break;
             case 4:
                 this.relatorios();
+                break;
             case 0:
                 System.exit(0);
         }
@@ -43,24 +46,119 @@ public class MenuView {
         String valor = JOptionPane.showInputDialog("JR Comercio de produtos LTDA.\n"
                 + "Controle de locadora de vídeo\n"
                 + "---------------------------------------------------\n"
-                + "Cadastro de " +tela+ "\n\n"
+                + "Cadastro de " + tela + "\n\n"
                 + "1 - Inclusão\n"
                 + "2 - Alteração\n"
                 + "3 - Consulta\n"
                 + "4 - Exclusão\n"
                 + "0 - Retornar");
-        
-        switch(option){
+        try {
+            option = Integer.parseInt(valor);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Você não botou nenhuma alternativa, tente novamente");
+            this.menu();
+        }
+
+        switch (option) {
             case 1:
                 if (tela.equalsIgnoreCase("Clientes")) {
-                    
-                }else{
+
+                } else {
                     filme.add();
                 }
+                break;
+            case 2:
+                if (tela.equalsIgnoreCase("Clientes")) {
+
+                } else {
+
+                }
+                break;
+            case 3:
+                if (tela.equalsIgnoreCase("Clientes")) {
+
+                } else {
+
+                }
+                break;
+            case 4:
+                if (tela.equalsIgnoreCase("Clientes")) {
+
+                } else {
+                    try {
+                        int id = Integer.parseInt(JOptionPane.showInputDialog("Digite o ID do filme:"));
+                        filme.delete(id);
+                    } catch (Exception e) {
+                        JOptionPane.showMessageDialog(null, "Id incorreto ou inexistente");
+                    }
+                }
+                break;
+            case 0:
+                this.menu();
+                break;
+            default:
+                this.menu();
+                break;
         }
     }
-    
-    
-    
-    
+
+    public void menuMovimentação() {
+        int option = 0;
+        String valor = JOptionPane.showInputDialog("JR Comercio de produtos LTDA.\n"
+                + "Controle de locadora de vídeo\n"
+                + "---------------------------------------------------\n"
+                + "Movimentação \n\n"
+                + "1 - Locação\n"
+                + "2 - Devolução\n"
+                + "0 - Retornar");
+        try {
+            option = Integer.parseInt(valor);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Você não botou nenhuma alternativa, tente novamente");
+            this.menuMovimentação();
+        }
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 0:
+                this.menu();
+                break;
+            default:
+                this.menu();
+                break;
+        }
+    }
+
+    public void relatorios() {
+        int option = 0;
+        String valor = JOptionPane.showInputDialog("JR Comercio de produtos LTDA.\n"
+                + "Controle de locadora de vídeo\n"
+                + "---------------------------------------------------\n"
+                + "Movimentação \n\n"
+                + "1 - Balanço do Acervo\n"
+                + "2 - Balanço de Movimentações\n"
+                + "0 - Retornar");
+
+        try {
+            option = Integer.parseInt(valor);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Você não botou nenhuma alternativa, tente novamente");
+            this.menuMovimentação();
+        }
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 0:
+                this.menu();
+                break;
+            default:
+                this.menu();
+                break;
+        }
+    }
+
 }
